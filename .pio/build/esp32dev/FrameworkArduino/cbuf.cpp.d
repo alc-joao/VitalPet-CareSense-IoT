d3b35d5e2a28d0bde80217b1c1d0bdd40f95fc52
Joao Victor Alcantara <joaovictoralc28@gmail.com>
2026-05-22 12:09:02 -0300

    Configurando projeto para rodar no PlatformIO

---
+++ .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ /Users/alc-joao/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /Users/alc-joao/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ .pio/build/esp32dev/FrameworkArduino/libb64/cencode.c.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/libb64/cencode.c.o: \
+ /Users/alc-joao/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
+ /Users/alc-joao/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
